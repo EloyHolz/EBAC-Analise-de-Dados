48a70e4cc7be7e7c32b407cfa62bb9b55e8912bd
Eloy Holzgrefe <eloyneto61@gmail.com>
2024-06-20 15:14:39 -0300

    Create Anotações-1.py

---
+++ Mod-01/Anotações-1.py
@@ -0,0 +1,49 @@
+#Primeira aula
+
+print("Olá, Mundo!")
+print('oba, eu quebrei a maldição, dnv kkk')
+
+#Segunda aula - estudo das Variáveis I
+
+joao_idade = 35
+print("Joao tem", joao_idade, "anos!")
+tipo_idade = type(joao_idade)
+print(tipo_idade)
+
+ana_idade = 10.6
+print("Já Ana tem", ana_idade, "anos")
+tipo_idade = type(ana_idade)
+print(tipo_idade)
+
+primeiro_nome = "Ana"
+print(primeiro_nome)
+print(type(primeiro_nome))
+
+questionamento = False
+print(questionamento)
+print(type(questionamento))
+
+valor = None
+print(None)
+print(type(None))
+
+#Aula 3 - Estudo Variáeis II
+
+compra = 0
+compra += 1
+print(compra)
+compra += 3
+print(compra)
+compra -= 2
+print(compra)
+
+preco_kg = 35
+peso = 0.5
+total = preco_kg * peso
+print(total)
+
+a = 3
+b = 2
+c = a/b
+d = a//b
+print("A divisão ")
