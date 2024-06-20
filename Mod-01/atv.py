7a778e5813f8a692188eff74cab49e5240595fb3
Eloy Holzgrefe <eloyneto61@gmail.com>
2024-06-20 19:39:14 -0300

    Create atv.py

---
+++ Mod-01/atv.py
@@ -0,0 +1,18 @@
+#atv 1
+print('olá mundo!')
+
+#atv 2
+tm_19 = 320.52
+vendas_19 = 3
+A = tm_19 * vendas_19
+print(A)
+
+total_20 = 834.47
+ticket_20 = 119.21
+b = total_20 / ticket_20
+print(b)
+
+vt_23 = 15378.12
+tt_23 = 5
+c = vt_23/tt_23
+print (c)
